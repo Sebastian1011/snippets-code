e4b3e4755184db407a854f0d96998f4c786afb1d
zmzhang <ziming.zhang@yitu-inc.com>
2018-07-14 16:08:22 +0800

    add proto

--- javascript/deepInJavascript/prototype.js
+++ javascript/deepInJavascript/prototype.js
@@ -10,14 +10,18 @@ console.log('person1 name: ', person1.name)
 
 console.log('person prototype === Person.prototype:', person.prototype === Person.prototype)
 console.log('person __proto__ === Person.prototype:', person.__proto__ === Person.prototype)
+console.log('Person === Person.prototype.constructor: ', Person === Person.prototype.constructor)
 
 console.log('Person :', Person)
 console.log('person prototype :', person.prototype)
 console.log('person __proto__ :', person.__proto__)
+
 
 /*** 
  * 结论：
  * Person 是构造函数
  * Person 的 prototype 是调用 Person 函数创建的实例的原型
  * 即 Person.prototype 是 person 和 Person1 的原型
+ * Person.prototype.constructor === Person
+ * 查找对象的属性先在对象上查找，找不到到对象的原型上找，还找不到就找原型的原型
  ***/
