fad76bc8fab83078034afe96db7a4f5dbb96b78b
zmzhang <ziming.zhang@yitu-inc.com>
2018-07-23 20:42:38 +0800

    merge array

---
+++ python/leetcode/mergeArray.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import unittest
+
+
+class Solution(unittest.TestCase):
+    def merge(self, nums1, m, nums2, n):
+        """
+        :type nums1: List[int]
+        :type m: int
+        :type nums2: List[int]
+        :type n: int
+        :rtype: void Do not return anything, modify nums1 in-place instead.
+        """
+        
+
+    def test(self):
+        pass
+
+if __name__ == '__main__':
+    s = Solution()
+    s.test()
